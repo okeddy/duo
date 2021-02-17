5c76e3ac2af90fb214b259ce85ee8e48845d6cb6
oudiak <2658495516@qq.com>
2021-02-17 22:26:02 +0800

    阻塞队列、循环阻塞队列

--- main.cc
+++ main.cc
@@ -1,25 +1,87 @@
-#include "thread/Mutex.h"
+#include "thread/BoundedBlockingQueue.h"
+#include "thread/CountDownLatch.h"
+#include "thread/Thread.h"
 
-class Request
+#include <boost/bind.hpp>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <string>
+#include <stdio.h>
+
+class Test
 {
- public:
-  void process() // __attribute__ ((noinline))
-  {
-    duo::MutexLockGuard lock(mutex_);
-    print();
-  }
+public:
+    Test(int numThreads)
+        : queue_(20),
+          latch_(numThreads),
+          threads_(numThreads)
+    {
+        for (int i = 0; i < numThreads; ++i)
+        {
+            char name[32];
+            snprintf(name, sizeof name, "work thread %d", i);
+            threads_.push_back(new duo::Thread(
+                boost::bind(&Test::threadFunc, this), std::string(name)));
+        }
+        for_each(threads_.begin(), threads_.end(), boost::bind(&duo::Thread::start, _1));
+    }
 
-  void print() const // __attribute__ ((noinline))
-  {
-    duo::MutexLockGuard lock(mutex_);
-  }
+    void run(int times)
+    {
+        printf("waiting for count down latch\n");
+        latch_.wait();
+        printf("all threads started\n");
+        for (int i = 0; i < times; ++i)
+        {
+            char buf[32];
+            snprintf(buf, sizeof buf, "hello %d", i);
+            queue_.put(buf);
+            printf("tid=%d, put data = %s, size = %zd\n", duo::CurrentThread::tid(), buf, queue_.size());
+        }
+    }
 
- private:
-  mutable duo::MutexLock mutex_;
+    void joinAll()
+    {
+        printf("============================\n");
+        for (size_t i = 0; i < threads_.size(); ++i)
+        {
+            queue_.put("stop");
+        }
+
+        for_each(threads_.begin(), threads_.end(), boost::bind(&duo::Thread::join, _1));
+    }
+
+private:
+    void threadFunc()
+    {
+        printf("tid=%d, %s started\n",
+               duo::CurrentThread::tid(),
+               duo::CurrentThread::name());
+
+        latch_.countDown();
+        bool running = true;
+        while (running)
+        {
+            std::string d(queue_.take());
+            printf("tid=%d, get data = %s, size = %zd\n", duo::CurrentThread::tid(), d.c_str(), queue_.size());
+            running = (d != "stop");
+        }
+
+        printf("tid=%d, %s stopped\n",
+               duo::CurrentThread::tid(),
+               duo::CurrentThread::name());
+    }
+
+    duo::BoundedBlockingQueue<std::string> queue_;
+    duo::CountDownLatch latch_;
+    boost::ptr_vector<duo::Thread> threads_;
 };
 
 int main()
 {
-  Request req;
-  req.process();
+    printf("pid=%d, tid=%d\n", ::getpid(), duo::CurrentThread::tid());
+    Test t(5);
+    t.run(50);
+    t.joinAll();
+
+    printf("number of created threads %d\n", duo::Thread::numCreated());
 }
---
+++ thread/BoundedBlockingQueue.h
@@ -0,0 +1,79 @@
+#ifndef DUO_BASE_BOUNDEDBLOCKINGQUEUE_H
+#define DUO_BASE_BOUNDEDBLOCKINGQUEUE_H
+
+#include "Condition.h"
+#include "Mutex.h"
+
+#include <boost/noncopyable.hpp>
+#include <boost/circular_buffer.hpp>
+#include <assert.h>
+
+namespace duo {
+    template<typename T>
+    class BoundedBlockingQueue : boost::noncopyable
+    {
+    public:
+        explicit BoundedBlockingQueue(int max_size)
+            :   mutex_(),
+                notFull_(mutex_),
+                notEmpty_(mutex_),
+                queue_(max_size)
+        { }
+
+        void put(const T& x) {
+            MutexLockGuard lock(mutex_);
+
+            while (queue_.full())
+            {
+                notFull_.wait();
+            }
+            
+            assert(!queue_.full());
+            queue_.push_back(x);
+            notEmpty_.notify();
+        }
+
+        T take() {
+            MutexLockGuard lock(mutex_);
+
+            while (queue_.empty())
+            {
+                notEmpty_.wait();
+            }
+            
+            assert(!queue_.empty());
+            T front(queue_.front());
+            queue_.pop_front();
+            notFull_.notify();
+            return front;
+        }
+
+        bool empty() const {
+            MutexLockGuard lock(mutex_);
+            return queue_.empty();
+        }
+
+        bool full() const {
+            MutexLockGuard lock(mutex_);
+            return queue_.full();
+        }
+
+        size_t size() const {
+            MutexLockGuard lock(mutex_);
+            return queue_.size();
+        }
+
+        size_t capacity() const {
+            MutexLockGuard lock(mutex_);
+            return queue_.capacity();
+        }
+
+    private:
+        mutable MutexLock mutex_;
+        Condition notFull_;
+        Condition notEmpty_;
+        boost::circular_buffer<T> queue_;
+    };
+}
+
+#endif  // DUO_BASE_BOUNDEDBLOCKINGQUEUE_H
